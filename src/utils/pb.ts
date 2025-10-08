80da357d2dc726b13a8b9f96bfc4ae84d2f641f4
BobelBanjamin <banjamin.bobel@edu.univ-fcomte.fr>
2025-10-08 18:07:40 +0300

    Update PocketBase path for production environment

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -4,7 +4,7 @@ var path = '';
 if (import.meta.env.MODE === 'development')
     path = 'http://localhost:8090'
 else
-    path = 'https://exam.banjamin-bobel.com/pb/api'
+    path = 'https://localhost:8085'
 
 const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
